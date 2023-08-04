6819e6054365a2bc71a6f4bf3b8667def8cb16d6
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-08-04 17:10:39 -0300

    Novas questões adicionadas

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -61,4 +61,66 @@ export const questions: Question[] = [
         options: ["Belém", "Jerusalém", "Nazaré", "Cafarnaum"],
         answer: 1,
     },
+    {
+        question:
+            "Qual dos seguintes patriarcas da Bíblia teve sonhos proféticos e foi vendido como escravo por seus irmãos?",
+        options: ["Abraão", "Jacó", "José", "Isaque"],
+        answer: 2, // Índice da resposta correta (0 para 'Abraão', 1 para 'Jacó', 2 para 'José', 3 para 'Isaque')
+    },
+    {
+        question:
+            "Quem foi o rei famoso por sua sabedoria e por construir o Templo em Jerusalém?",
+        options: ["Saul", "Davi", "Salomão", "Ezequias"],
+        answer: 2,
+    },
+    {
+        question: "Qual é o primeiro livro do Novo Testamento da Bíblia?",
+        options: ["Apocalipse", "Mateus", "João", "Lucas"],
+        answer: 1,
+    },
+    {
+        question:
+            "Qual dos seguintes profetas foi engolido por um grande peixe após tentar fugir da missão que Deus lhe deu?",
+        options: ["Jeremias", "Daniel", "Jonas", "Malaquias"],
+        answer: 2,
+    },
+    {
+        question:
+            "Quem foi a figura bíblica conhecida por sua paciência durante suas provações e aflições?",
+        options: ["Jó", "Noé", "Abraão", "Elias"],
+        answer: 0,
+    },
+    {
+        question: "Quais são os quatro evangelhos do Novo Testamento?",
+        options: [
+            "Mateus, Marcos, Lucas e João",
+            "Gênesis, Êxodo, Levítico e Números",
+            "Atos, Romanos, Coríntios e Gálatas",
+            "Hebreus, Tiago, Pedro e Judas",
+        ],
+        answer: 0,
+    },
+    {
+        question:
+            "Quem foi o profeta que desafiou os profetas de Baal para um duelo de sacrifícios no Monte Carmelo?",
+        options: ["Ezequiel", "Elias", "Jeremias", "Zacarias"],
+        answer: 1,
+    },
+    {
+        question:
+            "Qual dos seguintes apóstolos de Jesus era conhecido como o 'discípulo amado'?",
+        options: ["Pedro", "Tiago", "João", "André"],
+        answer: 2,
+    },
+    {
+        question: "Qual foi o fruto proibido que Eva comeu no Jardim do Éden?",
+        options: ["Pêra", "Maçã", "Uva", "Laranja"],
+        answer: 1,
+    },
+    {
+        question:
+            "Quem foi o líder carismático que conduziu os israelitas durante o Êxodo, guiando-os na travessia do Mar Vermelho?",
+        options: ["Moisés", "Josué", "Gideão", "Samuel"],
+        answer: 0,
+    },
 ];
